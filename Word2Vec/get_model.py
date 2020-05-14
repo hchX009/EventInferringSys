0d4c3b1e48df238109d3c150299c0d2b4c0d77bb
hchX009 <hecenhao@outlook.com>
2020-05-14 23:46:56 +0800

    comment 使用腾讯词向量 in 2020.05.14

--- Word2Vec/get_model.py
+++ Word2Vec/get_model.py
@@ -5,6 +5,10 @@
 # python 3.5
 
 from gensim.models import Word2Vec
+from gensim.models import KeyedVectors
+
+words_embedding_file = "../Data/Tencent_AILab_ChineseEmbedding/Tencent_AILab_ChineseEmbedding.txt"
+wv_from_text = KeyedVectors.load_word2vec_format(words_embedding_file, binary=False)
 
 sentences = [[],[]]
 
