1b5673d22612e0305a808c75812db1997f60ea56
hchX009 <hecenhao@outlook.com>
2020-05-18 21:53:14 +0800

    comment in 2020.05.18

--- EventAbstract/event_vector.py
+++ EventAbstract/event_vector.py
@@ -1,11 +1,25 @@
 # /usr/bin/env python3
 # coding: utf-8
-# File: get_model.py
+# File: event_vector.py
 # Author: hchX009
 # python 3.5
 
+
 from gensim.models import Word2Vec
 from gensim.models import KeyedVectors
+
+class EventVector:
+    def __init__(self):
+        pass
+
+    def get_event_from_triple(self):
+
+        return
+
+    def get_event_words(self, event):
+
+        return
+
 
 words_embedding_file = "../Data/Tencent_AILab_ChineseEmbedding/Tencent_AILab_ChineseEmbedding.txt"
 wv_from_text = KeyedVectors.load_word2vec_format(words_embedding_file, binary=False, limit=100000)
@@ -21,4 +35,5 @@ print(vector)
 
 # 使用默认CBOW算法，默认维数100
 #model = Word2Vec(sentences, min_count=1)
+
 
--- Word2Vec/get_event_vector.py
+++
@@ -1,8 +0,0 @@
-# /usr/bin/env python3
-# coding: utf-8
-# File: get_event_vector.py
-# Author: hchX009
-# python 3.5
-
-import gensim
-
