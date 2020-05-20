9eb3976fc35d94c65b5726100c32de5ec51bdfda
hchX009 <hecenhao@outlook.com>
2020-05-21 00:45:19 +0800

    comment mean of vector in event_vector.py in 2020.05.21

--- EventAbstract/event_vector.py
+++ EventAbstract/event_vector.py
@@ -5,17 +5,21 @@
 # python 3.5
 
 
-from gensim.models import Word2Vec
 from gensim.models import KeyedVectors
 from IO.database_operation import MongoOperation
 from NLP.ltp_parser import LtpParser
+import numpy
 
 
 class EventVector:
     def __init__(self):
         self.mongo_operation = MongoOperation()
         self.ltp_parser = LtpParser()
+        self.words_embedding_file = "../Data/Tencent_AILab_ChineseEmbedding/Tencent_AILab_ChineseEmbedding.txt"
+        self.wv_from_text = KeyedVectors.load_word2vec_format(self.words_embedding_file, binary=False, limit=100000)
+        self.wv_from_text.init_sims(replace=True)
 
+    # 从数据库中得到事件三组元按“,”划分的事件，并不重复
     def get_event_from_triple(self):
         event_triple_sets = self.mongo_operation.event_db_get()
         event_sets = list()
@@ -28,25 +32,20 @@ class EventVector:
                 event_sets.append(event)
         return event_sets
 
-    def get_event_words(self, event_sets):
+    # 得到事件的向量（词向量平均得到）
+    def get_event_vectors(self, event_sets):
         for event in event_sets:
             words_list = self.ltp_parser.get_words_by_pyltp(event)
-            print(words_list)
-        #words_embedding_file = "../Data/Tencent_AILab_ChineseEmbedding/Tencent_AILab_ChineseEmbedding.txt"
-        #wv_from_text = KeyedVectors.load_word2vec_format(words_embedding_file, binary=False, limit=100000)
-        #wv_from_text.init_sims(replace=True)
-
-        #word = '书记'
-
-        #vector = wv_from_text[word]
-
-        #print(vector)
-
-        # sentences = [[],[]]
-
-        # 使用默认CBOW算法，默认维数100
-        # model = Word2Vec(sentences, min_count=1)
-
+            # print(words_list)
+            # 建立长度向量长度的0向量
+            vector_sum = [0 for index in range(self.wv_from_text.wv.syn0[0].shape[0])]
+            for word in words_list:
+                vector = self.wv_from_text[word]
+                # print(vector)
+                vector_sum = list(numpy.array(vector_sum) + numpy.array(vector))
+            # print(vector_sum)
+            event_vector = [i / len(words_list) for i in vector_sum]
+            print(event_vector)
         return
 
 
@@ -54,4 +53,4 @@ if __name__ == "__main__":
     event_vector = EventVector()
     event_sets = event_vector.get_event_from_triple()
     print(event_sets)
-    event_vector.get_event_words(event_sets)
+    event_vector.get_event_vectors(event_sets)
